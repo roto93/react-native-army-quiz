5f3a1307cb474bd58daa9650231f550260d33674
roto93 <roto93@yahoo.com.tw>
2022-04-09 11:35:12 +0800

    :zap: Set up storage helper

---
+++ src/storage/storageHelper.js
@@ -0,0 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export const set = (key, value) => AsyncStorage.setItem(key, value)
+export const get = (key) => AsyncStorage.getItem(key)
+export const remove = (key) => AsyncStorage.removeItem(key)
+export const clear = () => AsyncStorage.clear()
